473bd02504d5c6c43317ec8a130c8e7c29c04ce9
Tim Mahy <timm@infosupport.com>
2018-10-11 18:42:39 +0200

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Microsoft.ML.Runtime.Learners;
 using Microsoft.ML.Runtime;
 using Microsoft.ML.Runtime.Data;
@@ -19,43 +16,47 @@ using Microsoft.ML.Transforms.Text;
 
 namespace BinaryClassification_SentimentAnalysis
 {
-
     internal static class Program
     {
-        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
 
         static void Main(string[] args)
         {
-            var env = new LocalEnvironment();
-            var classification = new MulticlassClassificationContext(env);
-            var reader = TextLoader.CreateReader(env, ctx => (Label: ctx.LoadText(0), Text: ctx.LoadText(1)), separator: ',');
-            var data = reader.Read(new MultiFileSource(@"C:\Users\timm\Source\Repos\ConsoleApp1\ConsoleApp1\datasets\data.tsv+C:\Users\timm\Source\Repos\ConsoleApp1\ConsoleApp1\datasets\61.tsv"));
-            
+            var testDataFile = TestData.PrepareTestDataAndReturnPath(23,61,72);
 
-            var learningPipeline = reader.MakeNewEstimator()
-                .Append(r => (Label: r.Label.ToKey(), Features: r.Text.FeaturizeText(advancedSettings: s =>
-                {
-                    s.KeepDiacritics = false;
-                    s.KeepNumbers = false;
-                    s.KeepPunctuations = false;
-                    s.TextCase = TextNormalizerTransform.CaseNormalizationMode.Lower;
-                    s.TextLanguage = TextTransform.Language.Dutch;
-                    s.VectorNormalizer = TextTransform.TextNormKind.LInf;
-                })))
-                .Append(r => (Label: r.Label, Predications: classification.Trainers.Sdca(r.Label, r.Features)));
+            for (int i = 0; i < 5; i++)
+            {
+                var env = new LocalEnvironment();
+                var classification = new MulticlassClassificationContext(env);
+                var reader = TextLoader.CreateReader(env, ctx => (Label: ctx.LoadText(0), Text: ctx.LoadText(1)), separator: ',', hasHeader: false);
+                var data = reader.Read(new MultiFileSource(testDataFile));
 
-            var (trainData, testData) = classification.TrainTestSplit(data, testFraction: 0.2);
-            var model = learningPipeline.Fit(trainData);
-            var metrics = classification.Evaluate(model.Transform(testData), r => r.Label, r => r.Predications);
-            Console.WriteLine(metrics.AccuracyMicro);
-            Console.WriteLine(metrics.AccuracyMacro);
+                var learningPipeline = reader.MakeNewEstimator()
+                    .Append(r => (Label: r.Label.ToKey(), Features: r.Text.FeaturizeText(advancedSettings: s =>
+                    {
+                        s.KeepDiacritics = false;
+                        //s.KeepNumbers = false; bv PC
+                        s.KeepPunctuations = false;
+                        s.TextCase = TextNormalizerTransform.CaseNormalizationMode.Lower;
+                        s.TextLanguage = TextTransform.Language.Dutch;
+                        s.VectorNormalizer = TextTransform.TextNormKind.LInf;
+                    })))
+                    .Append(r => (Label: r.Label, Predications: classification.Trainers.Sdca(r.Label, r.Features)));
 
-            var cvResults = classification.CrossValidate(data, learningPipeline, r => r.Label, numFolds: 5);
-            var microAccuracies = cvResults.Select(r => r.metrics.AccuracyMicro);
-            Console.WriteLine(microAccuracies.Average());
-            var macroAccuracies = cvResults.Select(r => r.metrics.AccuracyMacro);
-            Console.WriteLine(macroAccuracies.Average());
+                var (trainData, testData) = classification.TrainTestSplit(data, testFraction: 0.2);
+                var model = learningPipeline.Fit(trainData);
+                var metrics = classification.Evaluate(model.Transform(testData), r => r.Label, r => r.Predications);
+                Console.WriteLine(metrics.AccuracyMicro);
+                Console.WriteLine(metrics.AccuracyMacro);
+
+                //var cvResults = classification.CrossValidate(data, learningPipeline, r => r.Label, numFolds: 5);
+                //var microAccuracies = cvResults.Select(r => r.metrics.AccuracyMicro);
+                //Console.WriteLine(microAccuracies.Average());
+                //var macroAccuracies = cvResults.Select(r => r.metrics.AccuracyMacro);
+                //Console.WriteLine(macroAccuracies.Average());
+                Console.WriteLine("-----------");
+            }
         }
+
 
     }
 }
---
+++ ConsoleApp1/TestData.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BinaryClassification_SentimentAnalysis
+{
+    public static class TestData
+    {
+        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+
+        public static string PrepareTestDataAndReturnPath(params int[] additionalDataSets)
+        {
+            var orginalFilePath = Path.Combine(AppPath, "datasets", "data.csv");
+            var targetFilePath = Path.Combine(AppPath, "datasets", "preprocessed.csv");
+            if (File.Exists(targetFilePath))
+            {
+                File.Delete(targetFilePath);
+            }
+            using (var targetFile = File.Open(targetFilePath, FileMode.CreateNew))
+            using (var targetFileWriter = new StreamWriter(targetFile))
+            {
+                var lines = File.ReadLines(orginalFilePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length == 1)
+                    {
+                        Console.WriteLine("ERROR in file: " + parts[0]);
+                    }
+                    else
+                    {
+                        var label = parts[0];
+                        if (false == int.TryParse(label, out int x))
+                        {
+                            Console.WriteLine("ERROR in file : " + label);
+                        }
+                        var text = parts[1];
+                        var preprocessedLines = PreprocessAndReturnLineOrNull(label, text);
+                        foreach (var preprocessedLine in preprocessedLines)
+                        {
+                            if (false == string.IsNullOrWhiteSpace(preprocessedLine))
+                            {
+                                targetFileWriter.WriteLine(preprocessedLine);
+                            }
+                        }
+                    }
+                }
+            }
+
+            var paths = new List<string>();
+            paths.Add(targetFilePath);
+            var additionalPaths = PreprocessAdditionalDataSetsAndReturnPaths(additionalDataSets).ToArray();
+            paths.AddRange(additionalPaths);
+            paths.Add(Path.Combine(AppPath, "datasets", "classifications.csv"));
+            return string.Join('+', paths);
+        }
+
+        private static IEnumerable<string> PreprocessAdditionalDataSetsAndReturnPaths(int[] additionalDataSets)
+        {
+            if (additionalDataSets != null)
+            {
+                foreach (var additionalDataSet in additionalDataSets)
+                {
+                    var orginalFilePath = Path.Combine(AppPath, "datasets", $"{additionalDataSet}.csv");
+                    if (false == File.Exists(orginalFilePath))
+                    {
+                        Console.WriteLine("Path does not exist: " + orginalFilePath);
+                    }
+                    var targetFilePath = Path.Combine(AppPath, "datasets", $"{additionalDataSet}_preprocessed.csv");
+                    if (File.Exists(targetFilePath))
+                    {
+                        File.Delete(targetFilePath);
+                    }
+                    using (var targetFile = File.Open(targetFilePath, FileMode.CreateNew))
+                    using (var targetFileWriter = new StreamWriter(targetFile))
+                    {
+                        var lines = File.ReadLines(orginalFilePath);
+                        foreach (var line in lines)
+                        {
+                            targetFileWriter.WriteLine($"{additionalDataSet},{line}".ToLowerInvariant());
+                        }
+                    }
+                    yield return targetFilePath;
+                }
+            }
+        }
+
+        private static string[] BlockWords = new[] { "basiscursus", "postgraduaat", "-daagse", "graduaat", "opleiding", "inleiding" };
+
+        private static IEnumerable<string> PreprocessAndReturnLineOrNull(string label, string text)
+        {
+            // 1. alles naar lowercase
+            text = text.ToLowerInvariant();
+            // 2. bepaalde woorden die geen context bieden verwijderen
+            //foreach (var blockWord in BlockWords)
+            //{
+            //    text = text.Replace(blockWord, string.Empty);
+            //}
+            //// Domein specifieke
+            // Alles netjes trimmen nog
+            text = text.Trim();
+            yield return string.Join(',', label, text);
+        }
+    }
+}
